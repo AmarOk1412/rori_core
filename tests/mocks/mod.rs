27d933034bf012f08627e84b38a18ba69fffa5e6
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-03-31 11:42:25 -0400

    tests: add test for manager.rs

--- tests/mocks/mod.rs
+++ tests/mocks/mod.rs
@@ -1,8 +1,10 @@
-use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicBool, Ordering};
-use dbus::arg::Dict;
+use core::rori::account::Account;
+use dbus::arg::{Array, Dict};
 use dbus::{Connection, BusType, NameFlag, tree};
 use dbus::tree::Factory;
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
 
 // Our storage device
 #[derive(Debug)]
@@ -10,6 +12,8 @@ pub struct Storage {
     pub new_info: Arc<AtomicBool>,
     pub contacts_added: Vec<(String, String)>,
     pub interactions_sent: Vec<(String, String)>,
+    pub account: Arc<Mutex<Account>>,
+    pub accounts_added: Vec<HashMap<String, String>>,
 }
 
 // Every storage device has its own object path.
@@ -41,6 +45,12 @@ impl Daemon {
      * Init a daemon's mock
      */
     pub fn new() -> Daemon {
+        let glados_account = Account {
+            id: String::from("GLaDOs_id"),
+            ring_id: String::from("GLaDOs_ring_id"),
+            alias: String::from("GLaDOs"),
+            enabled: false
+        };
         Daemon {
             stop: Arc::new(AtomicBool::new(false)),
             initialized: Arc::new(AtomicBool::new(false)),
@@ -48,6 +58,8 @@ impl Daemon {
                 new_info: Arc::new(AtomicBool::new(false)),
                 contacts_added: Vec::new(),
                 interactions_sent: Vec::new(),
+                account: Arc::new(Mutex::new(glados_account)),
+                accounts_added: Vec::new(),
             })),
             emit_account_changed: Arc::new(AtomicBool::new(false)),
         }
@@ -80,6 +92,25 @@ impl Daemon {
                            .in_arg(("accountID", "s"))
                            .in_arg(("uri", "s"));
 
+       let add_account = f.method("addAccount", (), move |m| {
+                               let storage: &Arc<Mutex<Storage>> = m.path.get_data();
+                               let details = m.msg.get1::<Dict<&str, &str, _>>();
+                               let mut account_added: HashMap<String, String> = HashMap::new();
+                               for detail in details.unwrap() {
+                                   match detail {
+                                       (key, value) => {
+                                           account_added.insert(String::from(key), String::from(value));
+                                       }
+                                   }
+                               }
+                               storage.lock().unwrap().accounts_added.push(account_added);
+                               storage.lock().unwrap().new_info.store(true, Ordering::SeqCst);
+                               let rm = m.msg.method_return();
+                               let rm = rm.append1(storage.lock().unwrap().accounts_added.len().to_string());
+                               Ok(vec!(rm))
+                           })
+                          .in_arg(("details", "a{ss}"))
+                          .out_arg(("id", "s"));
 
        let send_text_message = f.method("sendTextMessage", (), move |m| {
                                let storage: &Arc<Mutex<Storage>> = m.path.get_data();
@@ -92,13 +123,90 @@ impl Daemon {
                           .in_arg(("accountID", "s"))
                           .in_arg(("uri", "s"));
 
+        let get_account_list = f.method("getAccountList", (), move |m| {
+                                     let storage: &Arc<Mutex<Storage>> = m.path.get_data();
+                                     let account = storage.lock().unwrap().account.clone();
+                                     let account = account.lock().unwrap();
+                                     let mut accounts: Vec<&str> = Vec::new();
+                                     accounts.push(&*account.id);
+                                     let details = Array::new(accounts.iter());
+                                     let rm = m.msg.method_return();
+                                     let rm = rm.append1(details);
+                                     Ok(vec!(rm))
+                                 })
+                                 .out_arg(("account_list", "as"));
+
+        let get_account_details = f.method("getAccountDetails", (), move |m| {
+                                       let storage: &Arc<Mutex<Storage>> = m.path.get_data();
+                                       let account_id = m.msg.get1::<&str>();
+                                       let account = storage.lock().unwrap().account.clone();
+                                       let account = account.lock().unwrap();
+                                       let mut details: HashMap<&str, &str> = HashMap::new();
+                                       if account.id == account_id.unwrap_or("") {
+                                           if account.enabled {
+                                               details.insert("Account.enable", "true");
+                                           } else {
+                                               details.insert("Account.enable", "false");
+                                           }
+                                           details.insert("Account.alias", &*account.alias);
+                                           details.insert("Account.username", &*account.ring_id);
+                                       }
+                                       let details = Dict::new(details.iter());
+                                       let rm = m.msg.method_return();
+                                       let rm = rm.append1(details);
+                                       Ok(vec!(rm))
+                                   })
+                                   .in_arg(("accountID", "s"))
+                                   .out_arg(("details", "a{ss}"));
+
+
+        let get_contacts = f.method("getContacts", (), move |m| {
+                                        let mut details = Vec::new();
+                                        let mut contact = HashMap::new();
+                                        contact.insert("id", "Atlas");
+                                        details.push(contact);
+                                        let mut contact = HashMap::new();
+                                        contact.insert("id", "PBody");
+                                        details.push(contact);
+                                        let mut contact = HashMap::new();
+                                        contact.insert("id", "Weasley");
+                                        details.push(contact);
+                                        let mut contact = HashMap::new();
+                                        contact.insert("id", "Space core");
+                                        details.push(contact);
+                                        let rm = m.msg.method_return();
+                                        let rm = rm.append1(details);
+                                        Ok(vec!(rm))
+                                    })
+                                    .in_arg(("accountID", "s"))
+                                    .out_arg(("details", "aa{ss}"));
+
+        let send_register = f.method("sendRegister", (), move |m| {
+                                  let storage: &Arc<Mutex<Storage>> = m.path.get_data();
+                                  let (account_id, enabled) = m.msg.get2::<&str, bool>();
+                                  let account = &storage.lock().unwrap().account;
+                                  let id = account.lock().unwrap().id.clone();
+                                  if id == account_id.unwrap_or("") {
+                                      account.lock().unwrap().enabled = enabled.unwrap_or(false);
+                                  }
+                                  let rm = m.msg.method_return();
+                                  Ok(vec!(rm))
+                              })
+                              .in_arg(("accountID", "s"))
+                              .in_arg(("enable", "b"));
+
         // We create a tree with one object path inside and make that path introspectable.
         let tree = f.tree(())
                     .add(f.object_path(configuration_path, storage.clone()).introspectable().add(
                         // We add an interface to the object path...
                         f.interface("cx.ring.Ring.ConfigurationManager", ())
                          .add_m(send_text_message)
+                         .add_m(add_account)
                          .add_m(add_contact)
+                         .add_m(get_account_list)
+                         .add_m(get_account_details)
+                         .add_m(get_contacts)
+                         .add_m(send_register)
                          .add_s(signal)
                     ));
 
--- tests/test_manager.rs
+++ tests/test_manager.rs
@@ -1,5 +1,127 @@
 extern crate core;
+extern crate dbus;
+extern crate time;
+
+mod mocks;
 
 #[cfg(test)]
-mod tests_api {
+mod tests_manager {
+    use core::rori::database::Database;
+    use core::rori::manager::Manager;
+    use mocks::Daemon;
+    use std::fs;
+    use std::sync::{Arc, Mutex};
+    use std::thread;
+
+    fn setup() {
+        let _ = fs::remove_file("rori.db");
+        Database::init_db(); // assert this function is correct.
+    }
+
+    fn teardown() {
+        let _ = fs::remove_file("rori.db");
+    }
+
+    #[test]
+    // Scenario
+    // 1. Ensures that a manager correctly enable and build an account
+    fn manager_init() {
+        setup();
+        let daemon = Arc::new(Mutex::new(Daemon::new()));
+        let cloned_daemon = daemon.clone();
+        let daemon_thread = thread::spawn(move|| {
+            Daemon::run(cloned_daemon);
+        });
+
+        // Test an account not here = Err(_)
+        let m = Manager::init("Heisenberg");
+        assert!(!m.is_ok());
+
+        let m = Manager::init("GLaDOs_id");
+        assert!(m.is_ok());
+        // GLaDOs_id should be enabled now.
+        let storage = daemon.lock().unwrap().storage.clone();
+        let account = storage.lock().unwrap().account.clone();
+        let account = account.lock().unwrap();
+        assert!(account.enabled == true);
+        assert!(account.id == "GLaDOs_id");
+
+        // Ensures that contacts are in server and in db.
+        assert!(Database::get_devices().len() == 4);
+        let devices = m.unwrap().server.anonymous_user.devices;
+        assert!(devices.len() == 4);
+        match devices.iter().find(|d| d.ring_id == "Atlas") {
+            Some(_) => {},
+            None => { panic!("Atlas not found") }
+        };
+        match devices.iter().find(|d| d.ring_id == "PBody") {
+            Some(_) => {},
+            None => { panic!("PBody not found") }
+        };
+        match devices.iter().find(|d| d.ring_id == "Weasley") {
+            Some(_) => {},
+            None => { panic!("Weasley not found") }
+        };
+        match devices.iter().find(|d| d.ring_id == "Space core") {
+            Some(_) => {},
+            None => { panic!("Space core not found") }
+        };
+
+        daemon.lock().unwrap().stop();
+        let _ = daemon_thread.join();
+        teardown();
+    }
+
+    #[test]
+    // Scenario
+    // 1. Retrieve account list
+    fn manager_get_account_list() {
+        setup();
+        let daemon = Arc::new(Mutex::new(Daemon::new()));
+        let cloned_daemon = daemon.clone();
+        let daemon_thread = thread::spawn(move|| {
+            Daemon::run(cloned_daemon);
+        });
+
+        let account_list = Manager::get_account_list();
+        assert!(account_list.len() == 1);
+        let account = account_list.first().unwrap();
+        assert!(account.id == "GLaDOs_id");
+        assert!(account.ring_id == "GLaDOs_ring_id");
+        assert!(account.alias == "GLaDOs");
+        assert!(!account.enabled);
+
+        daemon.lock().unwrap().stop();
+        let _ = daemon_thread.join();
+        teardown();
+    }
+
+    #[test]
+    // Scenario
+    // 1. Add an account from a path
+    // 2. Create a new account
+    fn manager_add_account() {
+        setup();
+        let daemon = Arc::new(Mutex::new(Daemon::new()));
+        let cloned_daemon = daemon.clone();
+        let daemon_thread = thread::spawn(move|| {
+            Daemon::run(cloned_daemon);
+        });
+        Manager::add_account("caroline.gz", "GLaDOs is a bad robot", true);
+        Manager::add_account("Cave Johnson", "Chell is my daughter", false);
+        let storage = daemon.lock().unwrap().storage.clone();
+        let accounts_added = storage.lock().unwrap().accounts_added.clone();
+        assert!(accounts_added.len() == 2);
+        let caroline = accounts_added.first().unwrap();
+        assert!(caroline.get("Account.archivePath").unwrap() == "caroline.gz");
+        assert!(caroline.get("Account.type").unwrap() == "RING");
+        assert!(caroline.get("Account.archivePassword").unwrap() == "GLaDOs is a bad robot");
+        let cave = accounts_added.last().unwrap();
+        assert!(cave.get("Account.alias").unwrap() == "Cave Johnson");
+        assert!(cave.get("Account.type").unwrap() == "RING");
+        assert!(cave.get("Account.archivePassword").unwrap() == "Chell is my daughter");
+        daemon.lock().unwrap().stop();
+        let _ = daemon_thread.join();
+        teardown();
+    }
 }
