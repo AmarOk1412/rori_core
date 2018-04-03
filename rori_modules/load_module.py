f69ab2ea6c487ae4909a684dc44d8063ad8a8f98
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-04-03 13:18:05 -0400

    rori_modules: improve load_module.py
    + use format strings
    + remove bad looking exec line and replace it with importlib.import

--- rori_modules/load_module.py
+++ rori_modules/load_module.py
@@ -1,25 +1,26 @@
-from rori import Interaction, Module
 import os.path
+import importlib
+from rori import Interaction, Module
 
 def load_module(path):
     '''import a module given a path'''
-    path = path.replace("/", ".")
-    if path[len(path)-1] == ".":
+    path = path.replace('/', '.')
+    if path[len(path)-1] == '.':
         path = path[:-1]
-    # TODO pass via importlib module to avoid the exec line
-    exec("import %s.module as module" % path, globals())
+    mod = importlib.import_module(f'{path}.module')
+    return mod
 
 def exec_module(path, interaction):
     '''execute a module given a path'''
     module_path = path
-    load_module(module_path)
-    if path[-1] == "/":
+    mod = load_module(module_path)
+    if path[-1] == '/':
         path = path[:-1]
-    path = "rori_modules/" + path + "/rsc.json"
-    sentences = "{}"
+    path = f'rori_modules/{path}/rsc.json'
+    sentences = '{}'
     if os.path.isfile(path):
         with open(path, 'r') as f:
             sentences = f.read()
-    m = module.Module(sentences)
+    m = mod.Module(sentences)
     m.process(Interaction(interaction))
     return m.continue_processing()
