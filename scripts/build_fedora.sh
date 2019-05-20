37eca12c6ddaaa8df1522d35a9d14a77aed47ab4
AmarOk <contact@enconn.fr>
2019-05-20 01:20:39 -0400

    build: fix install dependencies script

--- scripts/build_fedora.sh
+++ scripts/build_fedora.sh
@@ -2,8 +2,9 @@ sudo dnf update -y
 
 # Install ring-daemon
 sudo dnf install dnf-plugins-core -y
-sudo dnf config-manager --add-repo https://dl.ring.cx/ring-nightly/fedora_27/ring-nightly.repo
+sudo dnf config-manager --add-repo https://dl.ring.cx/ring-nightly/fedora_$(rpm -E %fedora)/ring-nightly.repo
 sudo dnf install ring -y # TODO replace by ring-daemon but doesn't seems to work
+sudo dnf install qt5-devel qt5-qtdeclarative qt5-qtdeclarative-devel qt5-qtbase qt5-qtbase-devel qt5-qtgraphicaleffects -y
 
 # Install Rust
 sudo dnf install rust cargo -y
